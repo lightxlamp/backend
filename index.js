d1c2da941957d05ac564dc6d94d349189e824398
lightxlamp <sheveliukhin.s@gmail.com>
2021-10-02 01:53:13 +0600

    feat: init server and home endpoint

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('Test Server...');
+
+import express from "express";
+
+const PORT = 5051;
+
+const app = express();
+
+// Creating Endpoint, that will handle GET query
+// 1st parameter is address, on which endpoint will work
+// 2nd parameter is function, that will be performed after "visiting" this endpoint
+app.get('/', (req, res)=> {
+    res.status(200).json('Server is working');
+});
+
+// Launching server
+// localhost:PORT
+app.listen(PORT, () => {
+    console.log('Server started');
+})
