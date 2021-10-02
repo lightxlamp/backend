818b775c6b8548b95edaedd5e93b1c325c414849
lightxlamp <sheveliukhin.s@gmail.com>
2021-10-03 00:10:20 +0600

    feat: create getPosts and getPost controllers

--- router.js
+++ router.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import PostController from "./PostController";
+import PostController from "./PostController.js";
 
 const router = new Router();
 
 router.post('/create_post', PostController.create);
-
-router.get('/posts');
-router.get('/posts/:id');
-router.put('/posts/:id');
-router.put('/delete/:id');
+router.get('/posts', PostController.getAll);
+router.get('/posts/:id', PostController.getOne);
+router.put('/posts/:id', PostController.update);
+router.delete('/delete/:id', PostController.delete);
 
 export default router;
