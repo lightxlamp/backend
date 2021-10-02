26915fdc51888e1ce58100cf156e47baa0bf6fc0
lightxlamp <sheveliukhin.s@gmail.com>
2021-10-02 23:27:59 +0600

    feat: move endpoint code to separate controller

--- router.js
+++ router.js
@@ -1,18 +1,9 @@
 import { Router } from "express";
-import Post from "./Post.js";
+import PostController from "./PostController";
 
 const router = new Router();
 
-router.post('/create_post', async (req, res) => {
-    try {
-        const {author, title, content, picture} = req.body;
-        const post = await Post.create({author, title, content, picture});
-        res.json(post)
-    }
-    catch(e) {
-        res.status(500).json(e);
-    }
-});
+router.post('/create_post', PostController.create);
 
 router.get('/posts');
 router.get('/posts/:id');
